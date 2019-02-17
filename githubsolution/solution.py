ee0f39f32fa3f150a5b7f5f7b56a08b3e9223b50
Traci Kamp <traci.kamp@gmail.com>
2019-02-17 13:09:07 -0600

    Token must be set via env var

--- githubsolution/solution.py
+++ githubsolution/solution.py
@@ -7,36 +7,73 @@ from discord import Embed
 class GithubSolution(Cog):
     """Posts a solution summary to a GitHub issue."""
     def __init__(self):
-        self.config = Config.get_conf(self, identifier=9900990099)
+        self.config = Config.get_conf(self, identifier=9900990099, force_registration=True)
         default_guild = {
-            "github_url": None
+            "github_project": None,
+            "github_token_var": None
         }
         self.config.register_guild(**default_guild)
 
     @command()
-    @checks.admin_or_permissions(manage_guild=True)
-    @checks.bot_in_a_guild()
-    async def setproject(self, ctx, project_url: str):
+    @checks.is_owner()
+    async def settokenvar(self, ctx, varname: str):
         """
-        Sets the GitHub URL to project_url.
+        The environment variable name used to retrieve the GitHub token.
+        Only the bot owner can set this value.
+        """
+        if not ctx.guild:
+            await ctx.send('You must be in a server channel to use this command.')
+            return
+        old_name = await self.config.guild(ctx.guild).github_token_var()
+        await self.config.guild(ctx.guild).github_token_var.set(varname)
+        await ctx.send(f'Value of `github_token_var` has changed from `{old_name}` to `{varname}`')
+
+    @command()
+    @checks.admin_or_permissions(manage_guild=True)
+    async def setproject(self, ctx, project: str):
+        """
+        Sets the GitHub project to use for this server.
+        Should be in the format namespace/project
         Default is None.
         """
         if not ctx.guild:
             await ctx.send('You must be in a server channel to use this command.')
             return
-        new_url = project_url.replace('<', '').replace('>', '')
-        old_url = await self.config.guild(ctx.guild).github_url()
-        await self.config.guild(ctx.guild).github_url.set(new_url)
-        await ctx.send(f'Value of `github_url` has changed from <{old_url}> to <{new_url}>')
+        old_project = await self.config.guild(ctx.guild).github_project()
+        await self.config.guild(ctx.guild).github_project.set(project)
+        await ctx.send(f'Value of `github_url` has changed from `{old_project}` to `{project}`')
 
     @command()
-    #@checks.role
     async def solution(self, ctx, issue: int = None, *, summary: str = None):
         """
         Posts a solution summary to a GitHub issue.
         Example: [p]solution 123 The user did not try turning it off/on again.
         """
-        embed = Embed(title=f'Solution to #{issue} posted!', colour=ctx.author.colour, url='https://github.com/tlkamp')
-        embed.add_field(name='Summary', value=summary)
-        embed.set_footer(text=f'Thanks, {ctx.author.name}!', icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
+        success = await self.do_post(issue, summary, ctx)
+        if success is None:
+            await ctx.send('The token could not be read. Please double check your configuration.')
+        else:
+            embed = Embed(title=f'Solution to #{issue} posted!', colour=ctx.author.colour, url=success)
+            embed.add_field(name='Summary', value=summary)
+            embed.set_footer(text=f'Thanks, {ctx.author.name}!', icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+
+    async def do_post(self, issue, solution, ctx):
+        text = [
+            '**Solution**',
+            f'{solution}',
+            '<hr>',
+            f'\n_This solution was posted by Discord user {ctx.author.name}._',
+            f'_[View this message on Discord.]({ctx.message.jump_url})_'
+        ]
+
+        varname = await self.config.guild(ctx.guild).github_token_var()
+        from os import environ
+        from github3 import login
+        token = environ.get(str(varname), None)
+        if token:
+            gh = login(token=token)
+            repo = await self.config.guild(ctx.guild).github_project()
+            iss = gh.issue(repo.split('/')[0], repo.split('/')[1], issue)
+            response = iss.create_comment('\n'.join(text))
+            return response.html_url
